9fa0a9d826c426f50eb713371d4c6b138ef73dbb
TTran2627 <39222229+TTran2627@users.noreply.github.com>
2019-12-16 22:04:28 -0600

    Clock frequency is 16 MHz not 84 Mhz

--- assignment08/Blinking_led_pwm/Src/main.c
+++ assignment08/Blinking_led_pwm/Src/main.c
@@ -196,7 +196,7 @@ static void MX_TIM2_Init(void)
   TIM_OC_InitTypeDef sConfigOC;
 
   htim2.Instance = TIM2;
-  htim2.Init.Prescaler = 84-1;
+  htim2.Init.Prescaler = 160-1; // clock run at 16 MHz
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
   htim2.Init.Period = 2000-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
